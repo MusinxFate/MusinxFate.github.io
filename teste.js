7592a3f752cac36d2d0183e7404110809c3c73aa
Murilo Barros <murilodesouzabarros@gmail.com>
2024-02-04 14:02:24 -0300

    Lot of Changes

--- teste.js
+++ teste.js
@@ -7,6 +7,24 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 40
+            },
+            1000: {
+                slidesPerView: 5,
+                spaceBetween: 50
+            }
+          }
     });
 });
 
@@ -17,7 +35,8 @@ const app = Vue.createApp({
             useFahrenheit: false,
             userTemperature: {},
             userLocation: '',
-            useSearch: false
+            useSearch: false,
+            imgUrl: ''
         }
     },
 
@@ -28,9 +47,10 @@ const app = Vue.createApp({
                     data = JSON.parse(data);
                     data.forEach(a => {
                         const parsedDate = new Date(a.date);
-                        a.date = parsedDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+                        a.date = parsedDate.toLocaleDateString('en-US', { weekday: 'long' });
                     });
                     this.userTemperature = data[0];
+                    this.imgUrl = data[0].imgUrl;
                     data.shift();
                     this.lstTemperatures = data;
                 });
@@ -46,12 +66,21 @@ const app = Vue.createApp({
         },
         changeSearch: function () {
             this.useSearch = this.useSearch === false ? true : false;
+        },
+        checkTimeDay: function() {
+            let date = new Date();
+            if (date.getHours() < 5 || date.getHours() > 18) {
+                this.imgUrl = 'assets/luna.png';
+            } else{
+                this.imgUrl = '/assets/sunny.png';
+            }
         }
     },
 
     beforeMount() {
-        this.findGeo();
+        this.checkTimeDay();
         this.requestWeather();
+        this.findGeo();
     }
 })
 
